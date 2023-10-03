9c8d79da09ca8c96ab2114e550a75bb72c5e867c
SeparPL <szymon.nieciag17@gmail.com>
2023-10-03 14:51:11 +0200

    date.php

--- date/date.php
+++ date/date.php
@@ -8,3 +8,11 @@ echo "Today is ".date("l")."<br>";
 <?php
 echo "The time is " . date("h:i:sa");
 ?>
+
+<!-- <?php
+$czas=mktime(11,14,54,8,12,2014);
+echo "Created date is ".date("d.m.y h:i:sa",$czas);
+$now=time();
+$week=7*24*60*60;
+echo "<br> teraz: ".date("d.m.Y h:i:sa", $now + $week);
+?> -->
