bf4a76369b9ba8bc38c1203edb155e122292670e
SeparPL <szymon.nieciag17@gmail.com>
2023-10-03 14:36:36 +0200

    date.php

---
+++ date/date.php
@@ -0,0 +1,10 @@
+<!-- <?php
+echo "Today is ".date("Y/m/d")."<br>";
+echo "Today is ".date("d.m.y")."<br>";
+echo "Today is ".date("d-m-y")."<br>";
+echo "Today is ".date("l")."<br>";
+?> -->
+
+<?php
+echo "The time is " . date("h:i:sa");
+?>
