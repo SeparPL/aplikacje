91bc9b986cb76f49680db2db8a16c53b3d1e419b
SeparPL <szymon.nieciag17@gmail.com>
2023-10-11 13:17:51 +0200

    walidacja emaili

---
+++ email/email.php
@@ -0,0 +1,30 @@
+
+<!-- 
+<?php
+$wzorzec='/([0123]?\d)+[-\.\/]+([01]?\d)+[-\.\/]+(\d{4})/';
+$tekst="11.09.2001";
+if(preg_match($wzorzec,$tekst,$matches))
+    echo "tak ".$matches[0];
+else
+    echo "nie";
+?> -->
+
+
+<!-- <?php
+$wzorzec='/[-\w.]+@([-A-z0-9]+\.)[A-z]+/';
+$tekst="szymon.pniaczek@wp.com email";
+if(preg_match($wzorzec,$tekst,$matches))
+    echo "tak ".$matches[0];
+else
+    echo "nie";
+?> -->
+
+
+<?php
+$wzorzec='/[-\w.]+@([-A-z0-9]+\.)[A-z]+/';
+$tekst="szymon.pniaczek@wp.com email";
+if(preg_match($wzorzec,$tekst,$matches))
+    echo "tak ".$matches[0];
+else
+    echo "nie";
+?>
